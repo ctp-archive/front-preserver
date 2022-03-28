0eb2174e22829aa35e9abaa1990524d75774fbca
Kevin Miller <keveemiller@gmail.com>
2022-03-28 11:29:23 -0700

    chore: Ditto

--- cmd.js
+++ cmd.js
@@ -6,6 +6,15 @@ import build from './src/index.js'
 const args = commandLineArgs([
   { name: 'source', alias: 's', type: String, defaultOption: true },
   { name: 'output', alias: 'o', type: String, defaultValue: './public' },
+  { name: 'dev', alias: 'd', type: Boolean, defaultValue: false },
+  { name: 'name', alias: 'n', type: String, defaultValue: 'Front export' },
+  { name: 'port', alias: 'p', type: String, defaultValue: 8080 },
+  {
+    name: 'templates',
+    alias: 't',
+    type: String,
+    defaultValue: './src/templates',
+  },
 ])
 
 build(args)
